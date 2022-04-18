7e4b3aacbae3e5d56e4ae92b116ffff7ea6345f4
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-04-18 20:09:32 +0530

    Time: 218 ms (54.54%), Space: 26.7 MB (32.83%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,23 +1,27 @@
 class Solution {
-    int dp[2][201][20005];
+  int dp[201][20005];  
 public:
     bool canPartition(vector<int>& nums) {
         memset(dp, -1, sizeof dp);
-        return canpartition(0, nums, 0);
+        int sum=0;
+        sum= accumulate(nums.begin(), nums.end(), 0);
+        if(sum%2!=0) return 0;
+        sum/=2;
+        return canpartition(nums.size()-1, nums, sum);
     }
      bool canpartition(int i, vector<int>& nums, int sum){
-         if(i==nums.size()){
-             if(sum==0) return 1;
-             else return 0;
+         if(i==0){
+             if(sum==0){
+                 return 1;
+             }else{
+                 return 0;
+             }
          }
-         int sign=1;
-         int t=sum;
-         if(sum<0){
-             sign=0;
-             t=-sum;
-         }
-         if(dp[sign][i][t]!=-1) return dp[sign][i][t]==1;
-         return dp[sign][i][t]=(canpartition(i+1, nums, sum+nums[i]) || canpartition(i+1, nums, sum-nums[i]));
+         if(sum<0) return 0;
+         if(dp[i][sum]!=-1) return dp[i][sum];
+          
+         return dp[i][sum]= (canpartition(i-1, nums, sum-nums[i]) || canpartition(i-1, nums, sum));
+         
         
      }
 };
