b969177fe90fc8b1169d48faffd9b31966548a46
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-04-18 16:02:40 +0530

    Time: 54 ms (94.39%), Space: 10.9 MB (77.69%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    int dp[12][10007];
+public:
+    int fun(int i, vector<int>& coins, int k){
+        if(k<0) return 1e7;
+        if(i==coins.size() && k==0) return 0;
+        if(i==coins.size() && k>0) return 1e7;
+        if(dp[i][k]!=-1) return dp[i][k];
+        int op1= 1+fun(i, coins, k-coins[i]);
+        int op2= fun(i+1, coins, k);
+       
+        return  dp[i][k]=min(op1, op2);
+    }
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        // sort(coins.begin(), coins.end(), greater<int>());
+        memset(dp, -1, sizeof dp);
+        fun(0, coins, amount);
+        return dp[0][amount]>=10000000? -1:dp[0][amount] ;
+    }
+};
