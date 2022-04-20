5ddd1cd02f69cad33aa097be5eccb9a8ec394cd9
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-04-20 20:02:44 +0530

    Time: 21 ms (43.51%), Space: 6.3 MB (98.74%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,22 +1,22 @@
 class Solution {
-private: int dp[501][501];
-public:
-    int L(int i, int j, string word1, string word2){
-        if(j==word2.size()) return word1.size()-i;
-        if(i==word1.size()) return word2.size()-j;
-        int op1, op2, op3;
-        if(dp[i][j]!=-1) return dp[i][j];
+// private: int dp[501][501];
+// public:
+//     int L(int i, int j, string word1, string word2){
+//         if(j==word2.size()) return word1.size()-i;
+//         if(i==word1.size()) return word2.size()-j;
+//         int op1, op2, op3;
+//         if(dp[i][j]!=-1) return dp[i][j];
         
-        if(word1[i]!=word2[j]) op1= 1+L(i+1, j+1, word1, word2);
-        else op1= L(i+1, j+1, word1, word2);
-        op2= 1+L(i, j+1, word1, word2);
-        op3=1+L(i+1, j, word1, word2);
-        return dp[i][j]=min(op1, min(op2, op3));
+//         if(word1[i]!=word2[j]) op1= 1+L(i+1, j+1, word1, word2);
+//         else op1= L(i+1, j+1, word1, word2);
+//         op2= 1+L(i, j+1, word1, word2);
+//         op3=1+L(i+1, j, word1, word2);
+//         return dp[i][j]=min(op1, min(op2, op3));
         
-    }
+//     }
 public:
     int minDistance(string word1, string word2) {
-        memset(dp, -1, sizeof dp);
+        // memset(dp, -1, sizeof dp);
         
         
         int x= word1.size();
