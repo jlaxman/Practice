e1bbb976758945a3f8f720fa8884cb57733d186c
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-05-03 21:32:23 +0530

    Added solution - LeetHub

--- Longest Common Subsequence - GFG/longest-common-subsequence.cpp
+++ Longest Common Subsequence - GFG/longest-common-subsequence.cpp
@@ -15,26 +15,39 @@ class Solution
         // your code here
         vector<vector<int>> dp(x+1, vector<int>(y+1, -1));
         
-        return LCS(0, 0, s1, s2, dp);
+        for(int i=0; i<=x; i++){
+            dp[i][y]=0;
+        }
+        for(int j=0; j<=y; j++){
+            dp[x][j]=0;
+        }
+        for(int i=x-1; i>=0; i--){
+            for(int j=y-1; j>=0; j--){
+                if(s1[i]==s2[j]) dp[i][j]=1+ dp[i+1][j+1];
+                else dp[i][j]=max(dp[i+1][j], dp[i][j+1]);
+            }
+        }
+        return dp[0][0];
+        
         
     }
-    int LCS(int i, int j, string& s1, string& s2,  vector<vector<int>>& dp){
-        if(i==s1.size() || j==s2.size()){
-          return 0;  
-        }
-        if(dp[i][j]!=-1) return dp[i][j];
-        int ans=0;
-        if(s1[i]==s2[j]){
-            ans=1+LCS(i+1, j+1, s1, s2, dp);
-            return dp[i][j]=ans;
-        }else{
-            int ans1= LCS(i+1, j, s1, s2, dp);
-            int ans2= LCS(i, j+1, s1, s2, dp);
-            return dp[i][j]=max(ans1, ans2);
+    // int LCS(int i, int j, string& s1, string& s2,  vector<vector<int>>& dp){
+    //     if(i==s1.size() || j==s2.size()){
+    //       return 0;  
+    //     }
+    //     if(dp[i][j]!=-1) return dp[i][j];
+    //     int ans=0;
+    //     if(s1[i]==s2[j]){
+    //         ans=1+LCS(i+1, j+1, s1, s2, dp);
+    //         return dp[i][j]=ans;
+    //     }else{
+    //         int ans1= LCS(i+1, j, s1, s2, dp);
+    //         int ans2= LCS(i, j+1, s1, s2, dp);
+    //         return dp[i][j]=max(ans1, ans2);
             
-        }
+    //     }
         
-    }
+    // }
 };
 
 
