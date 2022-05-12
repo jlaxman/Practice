39a0c5607e40342d3c2bfb30c22c8f1f07f5739f
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-05-12 11:54:16 +0530

    Time: 52 ms (19.68%), Space: 17.2 MB (11.06%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -1,6 +1,6 @@
 class Solution {
     
-    unordered_map<string, priority_queue<string, vector<string>, greater<string>>> mp;
+    // unordered_map<string, priority_queue<string, vector<string>, greater<string>>> mp;
     vector<string> vec;
     unordered_map<string, vector<string> > adj;
     unordered_map<string, int> mpp;
@@ -38,7 +38,7 @@ class Solution {
 public:
     vector<string> findItinerary(vector<vector<string>>& tickets) {
        
-        sort(tickets.begin(), tickets.end());
+        // sort(tickets.begin(), tickets.end());
         for(auto ticket: tickets){
             string from=ticket[0];
             string to=ticket[1];
@@ -47,12 +47,12 @@ public:
             mpp[temp]++;
         }
         
-        // for(auto dest: adj){
-        //     auto temp=dest.second;
-        //     sort(temp.begin(), temp.end());
-        //     adj[dest.first]=temp;
-        //     // cout<<t.first<<endl;
-        // }
+        for(auto dest: adj){
+            auto temp=dest.second;
+            sort(temp.begin(), temp.end());
+            adj[dest.first]=temp;
+            // cout<<t.first<<endl;
+        }
         
         int n=tickets.size()+1;
         string temp="JFK";
