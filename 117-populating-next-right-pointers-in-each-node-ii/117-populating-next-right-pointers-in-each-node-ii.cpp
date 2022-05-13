ccb54d355321745e99e577b5135bc6dcbe52bdf7
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-05-14 00:51:39 +0530

    Time: 34 ms (5.23%), Space: 17.4 MB (81.04%) - LeetHub

--- 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
+++ 117-populating-next-right-pointers-in-each-node-ii/117-populating-next-right-pointers-in-each-node-ii.cpp
@@ -20,21 +20,52 @@ class Solution {
 public:
     Node* connect(Node* root) {
         if(!root) return root;
-        queue<Node* > q;
-        q.push(root);
+        
        
-        while(!q.empty()){
-            int sz= q.size();
-            for(int i=0; i<sz; i++){
-                Node* currNode= q.front();
-                q.pop();
-                if(i!=sz-1) currNode->next= q.empty()? NULL: q.front();
-                if(currNode->left!=NULL)  q.push(currNode->left);
-                if(currNode->right!=NULL) q.push(currNode->right);
-                
-            }
+        Node* curr=root;
+        while(curr!=NULL){
+            
+        
+            Node* temp= new Node(0);
+            Node* nextfirst=NULL;
+                while(curr!=NULL){
+                   if(curr->left!=NULL && nextfirst==NULL){
+                       nextfirst=curr->left;
+                   }
+                   if(curr->right!=NULL && nextfirst==NULL){
+                       nextfirst=curr->right;
+                   }
+                    if(curr->left!=NULL){
+                        temp->next= curr->left;
+                        temp=temp->next;
+                    }
+                     if(curr->right!=NULL){
+                        temp->next= curr->right;
+                        temp=temp->next;
+                    }
+                    curr=curr->next;
+                 }
+            curr=nextfirst;
+            
         }
         return root;
+        
+        
+//         queue<Node* > q;
+//         q.push(root);
+       
+//         while(!q.empty()){
+//             int sz= q.size();
+//             for(int i=0; i<sz; i++){
+//                 Node* currNode= q.front();
+//                 q.pop();
+//                 if(i!=sz-1) currNode->next= q.empty()? NULL: q.front();
+//                 if(currNode->left!=NULL)  q.push(currNode->left);
+//                 if(currNode->right!=NULL) q.push(currNode->right);
+                
+//             }
+//         }
+//         return root;
         
     }
 };
