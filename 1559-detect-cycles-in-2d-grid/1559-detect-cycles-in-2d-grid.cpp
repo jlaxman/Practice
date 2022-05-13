17f5a748384e862da836935f93b863f8ebe2826c
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-05-13 14:15:55 +0530

    Time: 562 ms (19.85%), Space: 144.8 MB (17.96%) - LeetHub

---
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -0,0 +1,58 @@
+class Solution {
+public:
+    bool check(int x, int y, int m , int n){
+        
+        return (x>=0 && x<m && y>=0 && y<n);
+    }
+public:
+    bool  dfs(int node, vector<vector<int >>& adj, vector<bool>& visited, vector<bool>& stack, int parent){
+        visited[node]=true;
+        stack[node]=true;
+        
+        for(auto nbr: adj[node]){
+            if(visited[nbr]==0){
+                if(dfs(nbr, adj, visited, stack, node)==true) return true;
+            }else if( nbr!=parent && stack[nbr]==true){
+                 return true;
+            }
+        }
+        stack[node]=false;
+        return false;
+        
+        
+    }
+public:
+    bool containsCycle(vector<vector<char>>& grid) {
+        int m=grid.size();
+        int n=grid[0].size();
+        int dx[4]={0, 1, 0, -1};
+        int dy[4]={1, 0, -1, 0};
+        vector<bool> visited(m*n, false);
+        vector<bool> stack(m*n ,false);
+        vector<vector<int>> adj(m*n, vector<int>());
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                 for(int k=0; k<4; k++){
+                    int x=i+dx[k];
+                    int y=j+dy[k];
+                    if(check(x, y, m, n) && grid[x][y]==grid[i][j]){
+                        adj[i*n+j].push_back(x*n+y);
+                    }
+                     
+                 }
+                
+            }
+            
+        }
+        
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(visited[i*n+j]==false){
+                   if( dfs(i*n+j, adj, visited, stack, -1)) return true;
+                }
+            }
+        }
+        return false;
+        
+    }
+};
