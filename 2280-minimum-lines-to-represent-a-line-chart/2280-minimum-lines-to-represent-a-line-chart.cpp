e36d7f9e0798e2cf68f13a546b67801f84e54762
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-05-22 09:43:43 +0530

    Time: 520 ms (66.67%), Space: 103.9 MB (33.33%) - LeetHub

---
+++ 2280-minimum-lines-to-represent-a-line-chart/2280-minimum-lines-to-represent-a-line-chart.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minimumLines(vector<vector<int>>& sp) {
+        
+        
+        int n=sp.size();
+        if(n==1) return 0;
+        
+        sort(sp.begin(), sp.end());
+        vector<double > slope(n, 0);
+        int ans=0;
+        long double prev;
+        prev=INT_MAX;
+        
+        for(int i=1; i<n; i++){
+            // cout<< sp[i][0]<<" "<<sp[i][1]<<endl;
+            long double div=(1.0*(sp[i][1]-sp[i-1][1]));
+            long double divi=(1.0*(sp[i][0]-sp[i-1][0]));
+            // cout<< div<<" "<<divi<<endl;
+            long double curr= (long double) div/(1.0*divi);
+           
+            
+            if(curr!=prev) {
+                ans++;
+            }
+            prev= curr;
+          
+            
+            
+        }
+        return ans;
+        
+    }
+};
