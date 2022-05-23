c1738677552a10546c23cb9faf46ac39c031f85b
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-05-23 22:18:02 +0530

    Time: 164 ms (55.85%), Space: 43.8 MB (22.56%) - LeetHub

--- 743-network-delay-time/743-network-delay-time.cpp
+++ 743-network-delay-time/743-network-delay-time.cpp
@@ -4,28 +4,36 @@ public:
         
         vector<vector<pair<int, int>>> adj(n+1);
         for(auto t: times){
-            adj[t[0]].push_back(make_pair(t[2], t[1]));
+            adj[t[0]].push_back({t[1], t[2]});
         }
-       queue<int> q;
-        vector<int>  dist(n+1, 1e9);
+        
+      vector<int> dist(n+1, 1e9);
+      set<pair<int, int>> st;
+     st.insert({0, k});
         dist[k]=0;
-        dist[0]=0;
-        q.push(k);
-        while(!q.empty()){
-           auto p=q.front();
-           q.pop();
-            for(auto nbr: adj[p]){
-                if(dist[nbr.second]> nbr.first+ dist[p]){
-                    dist[nbr.second]=nbr.first+ dist[p];
-                    q.push(nbr.second);
+       int maxi=0;
+        while(st.size()){
+            auto it=st.begin();
+            int node=it->second;
+            st.erase(it);
+            for(auto nbr: adj[node]){
+                if(dist[nbr.first]> dist[node]+ nbr.second ){
+                    auto f=st.find({dist[nbr.first], nbr.first});
+                    if(f!=st.end()){
+                        st.erase(f);
+                    }
+                    
+                    dist[nbr.first]=dist[node]+nbr.second;
+                    st.insert({dist[nbr.first], nbr.first});
                 }
-                
             }
-          
         }
-        int ans=*max_element(dist.begin(), dist.end());
-        if(ans==1e9) return -1;
-        else return ans;
+        for(int i=1; i<=n; i++){
+            maxi=max(dist[i], maxi);
+            cout<< dist[i]<<" ";
+        }
+        if(maxi==1e9) return -1;
+        else return maxi;
        
         
     }
