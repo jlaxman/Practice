92af0b19153a8186820f5feaee1eaea89b5bcda7
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-05-26 16:28:11 +0530

    Time: 82 ms (79.86%), Space: 22.5 MB (70.56%) - LeetHub

--- 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -1,27 +1,30 @@
 class Solution{
     
 public: 
-    int fun(int i,  vector<int>& dp, vector<vector<int>>& pairs, int n){
-        if(dp[i]!=-1) return dp[i];
-        int ans=1;
-       for(int k=i+1; k<n; k++){
-           if(pairs[k][0]> pairs[i][1]){
-               ans= max(1+fun(k, dp, pairs, n), ans); 
-           }
-       }
-        return dp[i]=ans;
+//     int fun(int i,  vector<int>& dp, vector<vector<int>>& pairs, int n){
+//         if(dp[i]!=-1) return dp[i];
+//         int ans=1;
+//        for(int k=i+1; k<n; k++){
+//            if(pairs[k][0]> pairs[i][1]){
+//                ans= max(1+fun(k, dp, pairs, n), ans); 
+//            }
+//        }
+//         return dp[i]=ans;
         
-    }
+//     }
     int findLongestChain(vector<vector<int>>& pairs) {
         auto comp=[](auto& x, auto& y){
-            return x[0]==y[0]? x[1]>y[1]: x[0]< y[0];
+            return x[1]<y[1];
         };
         sort(pairs.begin(), pairs.end(), comp);
         int ans=1;
         int n=pairs.size();
-        vector<int> dp(n, -1);
-        for(int i=0; i<n; i++){
-            ans= max(fun(i, dp, pairs, n), ans);
+        int curr=pairs[0][1];
+        for(int i=1; i<n; i++){
+            if(curr<pairs[i][0]){
+                ans++;
+                curr=pairs[i][1];
+            }
         }
         return ans;
     }
