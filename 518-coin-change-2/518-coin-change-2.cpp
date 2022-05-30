bfcc5eed276fc2b1c2db3a8df729e35c69e4cc5d
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-05-30 11:40:31 +0530

    Time: 19 ms (71.18%), Space: 7.2 MB (75.24%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.cpp
+++ 518-coin-change-2/518-coin-change-2.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int change(int amt, vector<int>& coins) {
         vector<int> dp(amt+1, 0);
-        sort(coins.begin(), coins.end());
+        // sort(coins.begin(), coins.end());
         dp[0]=1;
         for(auto c: coins){
             for(int i=c;i<= amt; i++){
