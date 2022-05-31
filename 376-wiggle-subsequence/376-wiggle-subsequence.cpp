b02a496917480d3df4110a375df66a57bc6d65d7
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-05-31 23:24:07 +0530

    Time: 0 ms (100.00%), Space: 7.3 MB (16.70%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,35 +1,54 @@
 class Solution {
 public:
-    int fun(int i, int p, vector<int>& nums, vector<vector<int>>& dp, int n){
+//     int fun(int i, int p, vector<int>& nums, vector<vector<int>>& dp, int n){
         
-        if(dp[i][p]!=-1) return dp[i][p];
-        int ans=1;
-        if(p==0){
-           for(int k=i+1; k<n; k++){
-               if(nums[k]>nums[i]){
-                   ans=max(ans, 1+fun(k, 1-p, nums, dp, n));
-               }
-           }
+//         if(dp[i][p]!=-1) return dp[i][p];
+//         int ans=1;
+//         if(p==0){
+//            for(int k=i+1; k<n; k++){
+//                if(nums[k]>nums[i]){
+//                    ans=max(ans, 1+fun(k, 1-p, nums, dp, n));
+//                }
+//            }
         
-        }else {
-            for(int k=i+1; k<n; k++){
-               if(nums[k]<nums[i]){
-                   ans=max(ans, 1+fun(k, 1-p, nums, dp, n));
-               }
-           }
+//         }else {
+//             for(int k=i+1; k<n; k++){
+//                if(nums[k]<nums[i]){
+//                    ans=max(ans, 1+fun(k, 1-p, nums, dp, n));
+//                }
+//            }
             
-        }
-        return dp[i][p]=ans;
-    }
+//         }
+//         return dp[i][p]=ans;
+//     }
 public:
     int wiggleMaxLength(vector<int>& nums) {
         int n=nums.size();
-        vector<vector<int>> dp(nums.size(), vector<int>(2, -1));
-        int ans=0;
-        for(int i=0; i<n; i++){
-            ans=max(ans, fun(i, 0, nums, dp, n));
-            ans=max(ans, fun(i, 1, nums, dp, n));
+        // vector<vector<int>> dp(nums.size(), vector<int>(2, -1));
+        // int ans=0;
+        // for(int i=0; i<n; i++){
+        //     ans=max(ans, fun(i, 0, nums, dp, n));
+        //     ans=max(ans, fun(i, 1, nums, dp, n));
+        // }
+        // return ans;
+        
+        vector<int> dph(n, 0);
+        vector<int> dpl(n, 0);
+        dph[n-1]=1;
+        dpl[n-1]=1;
+        for(int i=n-2; i>=0; i--){
+            if(nums[i]>nums[i+1]){
+                dph[i]= 1+dpl[i+1];
+                dpl[i]=dpl[i+  1];
+            }else if(nums[i]<nums[i+1]){
+                dpl[i]= 1+dph[i+1];
+                dph[i]=dph[i+  1];
+            }else{
+                dph[i]=dph[i+1];
+                dpl[i]=dpl[i+1];
+            }
         }
-        return ans;
+        return max(dpl[0], dph[0]);
+        
     }
 };
