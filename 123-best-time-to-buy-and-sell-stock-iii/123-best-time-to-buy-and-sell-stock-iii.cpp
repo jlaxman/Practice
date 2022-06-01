06aea0437f98434a2e090064fe6232368caca249
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-01 18:30:11 +0530

    Time: 145 ms (93.68%), Space: 77.9 MB (62.42%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,30 +1,44 @@
 class Solution {
-int dp[100002][2][3];
+// int dp[100002][2][3];
+// public:
+//     int fun(int i, int state, int k, vector<int>& prices){
+//         if(k==0) return 0;
+//         if(i==prices.size()) return 0;
+//         if(dp[i][state][k]!=-1) return dp[i][state][k];
+//         if(state==1){
+//            return dp[i][state][k]=max(fun(i+1, 0, k, prices)-prices[i], fun(i+1, 1, k, prices));
+//         }else{
+//             return dp[i][state][k]=max(fun(i+1, 1, k-1, prices)+prices[i], fun(i+1, 0, k, prices));
+//         }
+//     }
 public:
-    int fun(int i, int state, int k, vector<int>& prices){
-        if(k==0) return 0;
-        if(i==prices.size()) return 0;
-        if(dp[i][state][k]!=-1) return dp[i][state][k];
-        if(state==1){
-           return dp[i][state][k]=max(fun(i+1, 0, k, prices)-prices[i], fun(i+1, 1, k, prices));
-        }else{
-            return dp[i][state][k]=max(fun(i+1, 1, k-1, prices)+prices[i], fun(i+1, 0, k, prices));
-        }
-    }
     int maxProfit(vector<int>& prices) {
-       // memset(dp, -1, sizeof dp);
-       // return fun(0, 1, 2, prices);
-        int t1cost=INT_MAX;
-        int t2cost=INT_MAX;
-        int p1=0;
-        int p2=0;
-        for(auto p: prices){
-            t1cost= min(p, t1cost);
-            p1=max(p1, p-t1cost);
-            t2cost=min(t2cost, p-p1);
-            p2= max(p2, p-t2cost);
+        int n=prices.size();
+        int dp[n+1][2][3];
+        memset(dp, -1, sizeof dp);
+        for(int i=0; i<2; i++){
+              for(int k=0; k<3; k++){
+                  dp[n][i][k]=0;
+              }
+         }
+        for(int i=0; i<=n; i++){
+            dp[i][0][0]=0;
+            dp[i][1][0]=0;
         }
-        return p2;
+        for(int i=n-1; i>=0; i--){
+            for(int s=0; s<2; s++){
+              for(int k=1; k<3; k++){
+                  if(s==1){
+                      dp[i][s][k]=max(dp[i+1][1-s][k]-prices[i], dp[i+1][s][k]);
+                  }else{
+                       dp[i][s][k]=max(dp[i+1][1-s][k-1]+prices[i], dp[i+1][s][k]);
+                  }
+              }
+            }     
+        }
+        
+        return dp[0][1][2];
+       
         
     }
 };
