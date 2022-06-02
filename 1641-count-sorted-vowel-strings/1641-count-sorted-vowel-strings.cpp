554eb5bdc454d261fd7a98e7afe969d403f37403
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-02 12:08:16 +0530

    Time: 3 ms (47.50%), Space: 6.5 MB (8.50%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.cpp
@@ -1,29 +1,27 @@
 class Solution {
     
-    int fun(int i, vector<char> & v, int n, string & temp){
-        if(i>4){
-            if(temp.size()==n) return 1;
-            return 0;
+public:
+    int countVowels(int n, int i, vector<vector<int>>& dp){
+        if(n==1){
+            return 1;
         }
-        if(temp.size()>n) return 0;
-        int op1=0;
-        int op2=0;
-        temp+=v[i];
-        op1=fun(i, v, n, temp);
-        temp.pop_back();
-        op2=fun(i+1, v, n, temp);
-        return op1+op2;
+        
+        if(dp[n][i]!=-1) return dp[n][i];
+        int ans=0;
+        for(int k=0; k<=i; k++){
+            ans+=countVowels(n-1, k, dp);
+        }
+        return dp[n][i]=ans;
+        
     }
 public:
     int countVowelStrings(int n) {
-        vector<char> v={'a', 'e', 'i', 'o', 'u'};
-        // v.push_back('a');
-        // v.push_back('e');
-        // v.push_back('i');
-        // v.push_back('o');
-        // v.push_back('u');
-        string temp;
-        return fun(0, v, n, temp);
-        
+        vector<vector<int>> dp(n+1, vector<int>(5, -1));
+       
+        int s=0;
+        for(int i: {0, 1, 2, 3, 4}){
+             s+=countVowels(n, i, dp);
+        }
+        return s;
     }
 };
