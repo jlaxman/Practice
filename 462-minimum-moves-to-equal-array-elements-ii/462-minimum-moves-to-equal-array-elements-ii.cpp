1019776a6f015c4a1160a8534745e360299c912e
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-02 16:48:06 +0530

    Time: 24 ms (12.86%), Space: 11.4 MB (5.27%) - LeetHub

---
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minMoves2(vector<int>& nums) {
+        int n=nums.size();
+        sort(nums.begin(), nums.end());
+        vector<long long> prefix(nums.begin(), nums.end());
+        for(int i=1;i<n; i++){
+            prefix[i]+=prefix[i-1];
+        }
+        long long misteps=INT_MAX;
+        for(long long i=0; i<n; i++){
+           long long currsteps=INT_MAX;
+            if(i!=0){
+                currsteps=abs(prefix[i-1]-  i*nums[i])+ abs(prefix[n-1]-prefix[i] -(n-(i+1))*nums[i]);
+                misteps= min(currsteps, misteps);
+            }else{
+                currsteps=abs(prefix[n-1]-prefix[i] -1LL*(n-(i+1))*nums[i]);
+                misteps=min(currsteps, misteps);
+            }
+        }
+        return misteps;
+        
+        
+    }
+};
