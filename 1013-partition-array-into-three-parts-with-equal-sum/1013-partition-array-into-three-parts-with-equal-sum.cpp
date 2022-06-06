29c4afb84f3d9cb26179c7487b043d00ee448986
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-07 00:23:06 +0530

    Time: 102 ms (9.98%), Space: 32.5 MB (60.86%) - LeetHub

---
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canThreePartsEqualSum(vector<int>& arr) {
+        int sum=accumulate(arr.begin(), arr.end(), 0);
+        if(sum%3) return 0;
+        int x=sum/3;
+        int ls=0;
+        int ifound=-1;
+        int jfound=-1;
+        for(int i=0; i<arr.size(); i++){
+            ls+=arr[i];
+            if(ls==x && ifound==-1){
+                ifound=i;
+            }
+            else if(ls==2*x && ifound!=-1 && jfound==-1){
+                jfound=i;
+                break;
+            }
+        }
+        if(jfound==-1 || jfound==arr.size()-1) return 0;
+        else if(ifound< jfound) {
+           return 1; 
+        }else return 0;
+    }
+};
