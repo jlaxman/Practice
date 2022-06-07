383e66037700023c16fba4c6887dca38fcab4089
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-07 11:35:59 +0530

    Time: 4 ms (63.80%), Space: 8 MB (34.86%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -4,14 +4,14 @@ public:
         vector<string> ans;
         for(int i=1; i<=n; i++){
             string temp;
-            if(i%3==0 && i%5==0){
-                temp="FizzBuzz";
-            }else if(i%3==0){
-                temp="Fizz";
-            }else if(i%5==0){
-                temp="Buzz";
-            }else{
-                temp=to_string(i);
+            if(i%3==0){
+                temp+="Fizz";
+            }
+            if(i%5==0){
+                temp+="Buzz";
+            }
+            if(temp==""){
+                temp+=to_string(i);
             }
             
             
