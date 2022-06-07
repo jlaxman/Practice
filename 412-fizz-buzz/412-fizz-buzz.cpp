dc300fc3eba5f56bb1143ad2a4bb135657dea483
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-07 11:37:45 +0530

    Time: 6 ms (38.19%), Space: 8 MB (34.86%) - LeetHub

--- 412-fizz-buzz/412-fizz-buzz.cpp
+++ 412-fizz-buzz/412-fizz-buzz.cpp
@@ -2,13 +2,15 @@ class Solution {
 public:
     vector<string> fizzBuzz(int n) {
         vector<string> ans;
+        map<int, string> mpp;
+        mpp[3]="Fizz";
+        mpp[5]="Buzz";
         for(int i=1; i<=n; i++){
             string temp;
-            if(i%3==0){
-                temp+="Fizz";
-            }
-            if(i%5==0){
-                temp+="Buzz";
+            for(auto m: mpp){
+                if(i%m.first==0){
+                    temp+=m.second;
+                }
             }
             if(temp==""){
                 temp+=to_string(i);
