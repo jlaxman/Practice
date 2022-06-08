320fcb8bf8f3484f40f14502f964a29f265094b2
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-08 13:00:05 +0530

    Time: 12 ms (49.14%), Space: 8.9 MB (68.07%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,47 +1,59 @@
 class Solution {
-    
-    class Graph{
-        int V;
-        list<int>* l;
-        public:
-        Graph(int V){
-            this->V=V;
-            l= new list<int> [V];
+   class dsu{
+        int n;
+        vector<int> parent;
+        vector<int> rank;
+    public:
+        dsu(int n){
+            this->n=n;
+            parent.resize(n, -1);
+            rank.resize(n, 1);
         }
-        
-        void addEdge(int i, int j){
-            l[i].push_back(j);
-            l[j].push_back(i);
+        int find_set(int i){
+            if(parent[i]==-1) return i;
+            else return parent[i]=find_set(parent[i]);
         }
-        
-        bool containscycle(int source, int out,  bool* visited, int parent){
-            visited[source]=true;
-            for(int nbr: l[source]){
-                if(visited[nbr]==false){
-                    if(containscycle(nbr, out, visited, source)==true) return true;
-                }
-                else if(nbr!=parent && nbr==out){
-                    return true;
+        void union_set(int i, int j){
+            int s1= find_set(i);
+            int s2=find_set(j);
+            if(s1!=s2){
+                if(rank[s1]<rank[s2]){
+                    rank[s2]+=rank[s1];
+                    parent[s2]=s1;
+                }else{
+                    rank[s1]+=rank[s2];
+                    parent[s1]=s2;
                 }
             }
-            return false;
-            
         }
+        bool givp(){
+            int cnt=0;
+           for(int i=1; i<n; i++){
+               if(parent[i]==-1) cnt++;
+           }
+        
+       return cnt==1;
+     }
+        
+        
+        
     };
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
-         Graph g(n+1);
-        for(auto e: edges){
-            g.addEdge(e[0], e[1]);
-        }
-       
-        for(int i=n-1; i>=0; i--){
-            bool* visited= new bool [n+1]{0};
-            visited[edges[i][1]]=true;
-            if(g.containscycle(edges[i][0], edges[i][1], visited, edges[i][1])==true) return {edges[i][0], edges[i][1]};
+          dsu d(n+1);
+        for(int i=0; i<n; i++){
+            int j=i;
+            int x=edges[j][0];
+            int y=edges[j][1];
+            int s1= d.find_set(x);
+            int s2= d.find_set(y);
+            if(s1!=s2){
+                d.union_set(x, y);
+            }else return edges[j];
+                  
         }
         return {};
-       
+        
     }
 };
