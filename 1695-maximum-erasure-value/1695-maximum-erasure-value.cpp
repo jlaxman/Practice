88fe9eccc03e8f61796de12d2e2546331e22f20b
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-12 12:30:48 +0530

    Time: 330 ms (78.25%), Space: 96.4 MB (91.62%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -11,15 +11,18 @@ public:
         int mx=0;
         for(int i=0; i<nums.size(); i++ ){
             if(freq[nums[i]]!=-1){
-                j=max(j, freq[nums[i]]+1);
+                j=max(j,freq[nums[i]]+1);
+                
             }
-            if(j!=0){
-               sum= prefix[i]-prefix[j-1];
-            }else{
-                sum= prefix[i];
-            }
-            mx=max(sum, mx);
             freq[nums[i]]=i;
+            if(j!=0){
+                    sum= prefix[i]-prefix[j-1];
+            }
+            else{
+                    sum=prefix[i];
+            }
+           mx=max(sum, mx);
+            
             
         }
         return mx;
