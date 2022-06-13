5f2ff79facb2877371fd5edc35f156752a6fea11
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-13 14:21:28 +0530

    Time: 419 ms (21.55%), Space: 10.4 MB (22.98%) - LeetHub

---
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minSteps(int n, vector<int>& ps, int k,  vector<int>& dp){
+        
+        if(k==0) return 0;
+        if(dp[k]!=-1) return dp[k];
+        int ans=INT_MAX;
+        int mini=INT_MAX;
+        for(int j=0; j<n; j++){
+            if(k>=ps[j]){
+                ans= minSteps(n, ps, k-ps[j], dp);
+                if(ans!=INT_MAX){
+                    ans++;
+                    mini=min(ans, mini);
+                } 
+            }
+        }
+        return dp[k]=mini;
+       
+       
+    }
+public:
+    int numSquares(int n) {
+        vector<int> ps;
+        for(int i=1; i<=sqrt(n); i++){
+            ps.push_back(i*i);
+        }
+     vector<int> dp(n+1, -1);
+        
+        return minSteps(ps.size(), ps, n, dp);
+        
+    }
+};
