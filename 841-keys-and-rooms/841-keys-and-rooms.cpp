3c895b8eecbf8a41bcc177cf51706572d6815115
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-15 12:24:00 +0530

    Time: 12 ms (65.59%), Space: 10.4 MB (79.60%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -1,81 +1,22 @@
 class Solution {
+    void dfs(int i, int n, vector<vector<int>>& rooms, vector<int>& vis){
+        vis[i]=1;
+        for(auto nbr: rooms[i]){
+            if(vis[nbr]==0){
+                dfs(nbr, n, rooms, vis);
+            }
+        }
+        return ;
+    }
 public:
-    
-class Graph {
-	int N;
-	list<int> *l;
-public:
-	Graph(int v) {
-		N = v;
-		l = new list<int> [v];
-	}
-	void addEdge(int i, int j, bool undir = true) {
-		l[i].push_back(j);
-		if (undir == true) {
-			l[j].push_back(i);
-		}
-	}
-	
-	bool bfs(int s) {
-		queue<int> q;
-// 		cout << s << endl;
-		q.push(s);
-
-		vector<int> visited(N, 1e9);
-		visited[s] = 0;
-
-		while (!q.empty()) {
-			int curr = q.front();
-			q.pop();
-
-			for (int nbr : l[curr]) {
-				if (visited[nbr] == 1e9) {
-					// cout << nbr << endl;
-					visited[nbr] = visited[curr] + 1;
-				// 	cout << nbr << " " << visited[nbr] << endl;
-					q.push(nbr);
-				}
-			}
-		}
-		for(auto c: visited){
-		    if(c==1e9) return false;
-		}
-		return true;
-
-	}
-	void dfs_helper(int source, bool* visited) {
-
-		visited[source] = true;
-// 		cout << source << ',';
-		for (auto nbr : l[source]) {
-			if (visited[nbr] == false) {
-				dfs_helper(nbr, visited);
-			}
-		}
-	}
-
-	bool dfs(int source) {
-		bool* visited = new bool [N];
-		for (int i = 0; i < N; i++) visited[i] = false;
-		dfs_helper(source, visited);
-		for(int i=0; i<N; i++ ){
-		    if(visited[i]==false) return false;
-		}
-		return true;
-	}
-
-
-};
-bool canVisitAllRooms(vector<vector<int>> rooms) {
-   Graph g(rooms.size());
-   int j=0;
-   for(auto room: rooms){
-       for(auto key: room){
-           g.addEdge(j, key, false);
-       }
-       j++;
-   }
-   return g.dfs(0);
-   
-}
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n=rooms.size();
+        vector<int> vis(n, 0);
+        dfs(0, n, rooms, vis);
+        for(int i=0; i<n; i++){
+            if(vis[i]==0) return 0;
+        }
+        return 1;
+        
+    }
 };
