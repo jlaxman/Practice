44d8902e72e24f66a6ca3319cb914ba04bb1b840
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-21 02:01:46 +0530

    Time: 154 ms (35.29%), Space: 92 MB (5.39%) - LeetHub

--- 820-short-encoding-of-words/820-short-encoding-of-words.cpp
+++ 820-short-encoding-of-words/820-short-encoding-of-words.cpp
@@ -1,20 +1,50 @@
 class Solution {
 public:
-    int minimumLengthEncoding(vector<string>& words) {
-        unordered_set<string> st(words.begin(), words.end());
-        for(auto w: words){
-            for(int i=1; i<w.size(); i++){
-                auto f=st.find(w.substr(i));
-                if(f!=st.end()){
-                    st.erase(f);
-                }
+    class Node{
+    public:
+        string word;
+        vector<Node*> next{vector<Node*> (26, NULL)};
+        Node(string s){
+            word=s;
+        }
+    };
+   int ans;
+    void insert(string s, Node* root){
+        Node* curr= root;
+        string temp;
+        for(auto ch: s){
+            temp+=ch;
+            if(curr->next[ch-'a']==NULL){
+                curr->next[ch-'a']= new Node(temp);
+            }
+            curr=curr->next[ch-'a'];
+        }
+    }
+    void dfs(Node* curr){
+        
+        int flag=0;
+        for(int i=0; i<26; i++){
+            if(curr->next[i]!=NULL){
+                dfs(curr->next[i]);
+                flag=1;
             }
         }
-        int ans=0;
-        for(auto it=st.begin(); it!=st.end(); it++){
-            ans+=(*it).size()+1;
+        if(flag==0) ans+=(((curr->word).size())+1);
+        return ;
+        
+    }
+    
+    
+    int minimumLengthEncoding(vector<string>& words) {
+        Node* root= new Node("");
+        ans=0;
+        for(auto w: words){
+            reverse(w.begin(), w.end());
+            insert(w, root);
         }
+        dfs(root);
         return ans;
+        
         
   
     }
