88eec44bfed43a9604f8d8255556135ac5ed6dd7
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-22 21:03:29 +0530

    Time: 1123 ms, Memory: 740.7 MB - LeetHub

---
+++ add-and-search-word-data-structure-design/add-and-search-word-data-structure-design.cpp
@@ -0,0 +1,66 @@
+class WordDictionary {
+public:
+    class Node{
+        public:
+        string word;
+        bool isend;
+        vector<Node*> next{vector<Node*> (26, NULL)};
+        Node(string s){
+            word=s;
+            isend=false;
+        }
+    };
+    // unordered_set<string> st;
+    int mx;
+    Node* root;
+    WordDictionary() {
+        root=new Node("");
+        mx=0;
+    }
+    
+    void addWord(string word) {
+       Node* curr=root;
+        string temp;
+        for(auto ch: word){
+            temp+=ch;
+            if(curr->next[ch-'a']==NULL){
+                curr->next[ch-'a']= new Node(temp);
+            }
+            curr=curr->next[ch-'a'];
+        }
+        curr->isend=true;
+        mx=max((int)word.size(), mx);
+    }
+    
+    bool search(string word) {
+        if(word.size()>mx) return 0;
+         return dfs(root, word, 0);
+    }
+    bool dfs(Node* curr, string& word, int i){
+        if(i==word.size()){
+            return curr->isend;
+        }
+        if(word[i]=='.'){
+            
+            for(int k=0; k<26; k++){
+                if(curr->next[k]!=NULL){
+                    if(dfs(curr->next[k], word, i+1)==1) return 1;
+                }
+            }
+            
+        }else{
+            if(curr->next[word[i]-'a']!=NULL){
+                if(dfs(curr->next[word[i]-'a'], word, i+1)==1) return 1;
+        
+            }else return 0;
+        }
+        return 0;
+    }
+};
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary* obj = new WordDictionary();
+ * obj->addWord(word);
+ * bool param_2 = obj->search(word);
+ */
