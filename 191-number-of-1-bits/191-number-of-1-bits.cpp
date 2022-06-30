729d5d63065ddae4ddbd4116b3e39c626c1f852d
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-06-30 13:27:50 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (47.54%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -2,9 +2,11 @@ class Solution {
 public:
     int hammingWeight(uint32_t n) {
         int cnt=0;
-        for(int i=31; i>=0; i--){
-            if(n & (1<<i)) cnt++;
-        }
+        // for(int i=31; i>=0; i--){
+        //     if(n & (1<<i)) cnt++;
+        // }
+        cnt= __builtin_popcount(n);
+        
         return cnt;
         
     }
