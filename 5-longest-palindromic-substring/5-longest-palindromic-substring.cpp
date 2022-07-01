a4e15950cdd4074a88c86b850c99f0612427663f
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-07-01 10:54:26 +0530

    Time: 638 ms (21.30%), Space: 29.3 MB (37.13%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -10,7 +10,7 @@ public:
         for(int i=0; i<n; i++) dp[i][i]=1;
         for(int i=0; i<n-1; i++){
             if(s[i]==s[i+1]){
-                dp[i][i+1]=2;
+                dp[i][i+1]=1;
                 ans=s.substr(i, 2);
             }
         }
@@ -18,10 +18,10 @@ public:
         for(int l=3; l<=n; l++){
             for(int i=0; i<=n-l; i++){
                 if(s[i]==s[i+l-1] && dp[i+1][i+l-2]){
-                    dp[i][i+l-1]=l;
+                    dp[i][i+l-1]=1;
                 }
-                if(dp[i][i+l-1]>maxl){
-                    maxl=dp[i][i+l-1];
+                if(dp[i][i+l-1] && l>maxl){
+                    maxl=l;
                     ans=s.substr(i, l);
                 }
             }
