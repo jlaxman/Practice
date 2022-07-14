432d6a56b1dcff2bcf92b64c44b7f835f2caab2b
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-07-14 12:49:45 +0530

    Time: 4 ms (59.19%), Space: 8.6 MB (42.75%) - LeetHub

--- 274-h-index/274-h-index.cpp
+++ 274-h-index/274-h-index.cpp
@@ -11,32 +11,32 @@ public:
         return cnt>=m;
     }
     int hIndex(vector<int>& c) {
-//         int l= *min_element(c.begin(), c.end());
-//          int r= *max_element(c.begin(), c.end());
-//         l=min(l, (int)c.size());
-//         r= min(r, (int)c.size());
-//         int ans=0;
-//         while(l<=r){
-//             int mid=(l+r)/2;
-//             if(predicate(mid, c)){
-//                 l=mid+1;
-//                 ans=mid;
-//             }else{
-//                 r=mid-1;
-//             }
-            
-//         }
-//         return ans;
-        
-        
-        sort(c.begin(), c.end());
+        int l= *min_element(c.begin(), c.end());
+         int r= *max_element(c.begin(), c.end());
+        l=0;
+        r=c.size();
         int ans=0;
-        for(int i=0; i<c.size(); i++){
-            int r= c.size()-i;
-            ans=max(ans, min(r, c[i]));
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(predicate(mid, c)){
+                l=mid+1;
+                ans=mid;
+            }else{
+                r=mid-1;
+            }
             
         }
         return ans;
+        
+        
+//         sort(c.begin(), c.end());
+//         int ans=0;
+//         for(int i=0; i<c.size(); i++){
+//             int r= c.size()-i;
+//             ans=max(ans, min(r, c[i]));
+            
+//         }
+//         return ans;
         
     }
 };
