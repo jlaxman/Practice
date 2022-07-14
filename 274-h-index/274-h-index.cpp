7ff003b715339b2e261da28c66c66bd3cba70a61
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-07-14 12:23:28 +0530

    Time: 9 ms (17.90%), Space: 8.7 MB (42.75%) - LeetHub

---
+++ 274-h-index/274-h-index.cpp
@@ -0,0 +1,32 @@
+class Solution {
+
+public:
+    bool predicate(int m, vector<int>& c){
+        int cnt=0;
+        for(auto n: c){
+            if(n>=m){
+                cnt++;
+            }
+        }
+        return cnt>=m;
+    }
+    int hIndex(vector<int>& c) {
+        int l= *min_element(c.begin(), c.end());
+         int r= *max_element(c.begin(), c.end());
+        l=min(l, (int)c.size());
+        r= min(r, (int)c.size());
+        int ans=0;
+        while(l<=r){
+            int mid=(l+r)/2;
+            if(predicate(mid, c)){
+                l=mid+1;
+                ans=mid;
+            }else{
+                r=mid-1;
+            }
+            
+        }
+        return ans;
+        
+    }
+};
