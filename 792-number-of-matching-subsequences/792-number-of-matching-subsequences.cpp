842b04dce9d0e562ece22cc350a9651387441e27
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-07-20 14:50:27 +0530

    Time: 1114 ms (11.85%), Space: 409.6 MB (5.20%) - LeetHub

--- 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
+++ 792-number-of-matching-subsequences/792-number-of-matching-subsequences.cpp
@@ -1,37 +1,78 @@
 class Solution {
 public:
     int numMatchingSubseq(string s, vector<string>& words) {
-        vector<vector<int>> q(26, vector<int>());
-        for(int i=0;i<s.size(); i++){
-            q[s[i]-'a'].push_back(i);
-        }
-        int cnt=0;
-        for(auto w: words){
-            
-            
-            int flag=0;
-            int prev=-1;
-            for(auto ch: w){
-                
-                if(q[ch-'a'].size()==0){
-                    flag=1;
-                    break;
-                }
-                int ind=upper_bound(q[ch-'a'].begin(), q[ch-'a'].end(), prev)-q[ch-'a'].begin();
-                if(ind==q[ch-'a'].size()){
-                    flag=1;
-                    break;
-                }else{
-                    prev=q[ch-'a'][ind];
-                }
+        
+          vector<int> curr(26, -1);
+          vector<vector<int>> next;
+          for(int i=s.size()-1; i>=0; i--){
+              next.push_back(curr);
+              curr[s[i]-'a']=i;
+          }
+          next.push_back(curr);
+          reverse(next.begin(), next.end());
+          int cnt=0;
+          for(auto w: words){
+              int i=-1;
+              int flag=0;
+              for(auto ch: w){
+                  if(next[i+1][ch-'a']==-1){
+                      flag=1;
+                      break;
+                  }else{
+                      i= next[i+1][ch-'a'];
+                  }
+                   
+               }
+              if(flag==0){
+                  cnt++;
+              }
             }
-            if(flag==0){
-                cnt++;
-            }
-            
-            
-        }
         return cnt;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//         vector<vector<int>> q(26, vector<int>());
+//         for(int i=0;i<s.size(); i++){
+//             q[s[i]-'a'].push_back(i);
+//         }
+//         int cnt=0;
+//         for(auto w: words){
+//             int flag=0;
+//             int prev=-1;
+//             for(auto ch: w){
+//                 if(q[ch-'a'].size()==0){
+//                     flag=1;
+//                     break;
+//                 }
+//                 int ind=upper_bound(q[ch-'a'].begin(), q[ch-'a'].end(), prev)-q[ch-'a'].begin();
+//                 if(ind==q[ch-'a'].size()){
+//                     flag=1;
+//                     break;
+//                 }else{
+//                     prev=q[ch-'a'][ind];
+//                 }
+//             }
+//             if(flag==0){
+//                 cnt++;
+//             } 
+//         }
+//         return cnt;
         
     }
 };
