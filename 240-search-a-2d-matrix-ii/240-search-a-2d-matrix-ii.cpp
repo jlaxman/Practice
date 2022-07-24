487e1ed20c28a8a12bf8510cceecb12afd4dee3c
jlaxman <47687813+jlaxman@users.noreply.github.com>
2022-07-25 00:14:18 +0530

    Time: 634 ms (5.93%), Space: 14.7 MB (98.00%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     bool searchMatrix(vector<vector<int>>& matrix, int target) {
-        for(auto &v: matrix){
-            for(auto &c: v){
-                if(c==target) return 1;
-            }
+        // for(auto &v: matrix){
+        //     for(auto &c: v){
+        //         if(c==target) return 1;
+        //     }
+        // }
+        // return 0;
+        for(int i=0; i<matrix.size(); i++){
+            auto lb=lower_bound(matrix[i].begin(), matrix[i].end(), target);
+            if(lb!=matrix[i].end() && *lb==target) return 1;
         }
         return 0;
+        
         
     }
 };
